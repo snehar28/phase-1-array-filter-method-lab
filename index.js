97d9a2faf37bff0b935fc1da2f8b80b4b489493e
+ <+>
2022-07-27 13:47:03 +0100

    commit tree fbf3b51d1da4e1a9e7d9e3a9126c574f86a5e5a8

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(collection, name){
+    return collection.filter(function(driver){
+        return driver.toLowerCase() == name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(collection, letters){
+    const newArray = [];
+    return collection.filter(function(driver){
+        if (driver.startsWith(letters)){
+            return newArray.push(driver)
+        }
+    })
+}
+
+function matchName(collection, string){
+    return collection.filter(function(driver){
+        if (driver.name === string){
+            return driver;
+        }
+    })
+}
